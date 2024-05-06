a2e9cceac5df25ae50ccd8dbe242b36ade962867
mariams58 <mariams58@yahoo.com>
2024-05-06 13:22:56 +0100

    0x0C task 7 fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -3,7 +3,8 @@ exec {'configure':
   provider => shell,
   command  => 'sudo apt-get -y update; sudo apt-get install nginx;
   echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html;
-  sudo sed -i "s/server_name _ ;/server_name_;\n\t
-  rewrite ^\/redirect_me rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/" /etc/nginx/sites-available/default; sudo service nginx start',
+  sudo sed -i "s/server_name _ ;/server_name _;\n\t
+  rewrite ^\/redirect_me rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;/"
+   /etc/nginx/sites-available/default; sudo service nginx start',
 
 }
