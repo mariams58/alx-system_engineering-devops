44efc7bddcf31904a36415c1143fceafcc89cac1
mariams58 <mariams58@yahoo.com>
2024-05-28 13:07:49 +0100

    puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,12 +1,12 @@
 #install Nginx in a server and configure it
 exec {'configure':
   provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; ufw allow Nginx HTTP; chmod -R 755 /var/www',
 }
 
 file_line { 'red':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
+  path   => '/etc/nginx/sites-enabled/default',
   after  => 'listen 80 default_server;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
