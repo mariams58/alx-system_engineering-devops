a757797101db3f35e031c4bb13a38e883af8fabf
mariams58 <mariams58@yahoo.com>
2024-06-10 14:06:14 +0100

    load balancing - task 2 fix 6

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,8 @@
 # Creates a custom hader in an nginx server
-#install Nginx in a server and configure it
-exec {'configure':
+exec { 'header':
+  command => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
   provider => shell,
-  command  => 'sudo apt-get -y update',
-}
-
-# Install Nginx
-package {'nginx':
-  ensure => installed,
-}
-
-# Custom header
-file_line {'add_header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By $HOSTNAME;',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
-}
-
-#start service
-service {'nginx':
-  ensure    => 'running',
-  enable    => true,
-  require   => Package['nginx'],
-  subscribe => File_line['add_header'],
 }
---
+++ 0x0F-load_balancer/temp.pp
@@ -0,0 +1,29 @@
+# Creates a custom hader in an nginx server
+#install Nginx in a server and configure it
+exec {'configure':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+}
+
+# Install Nginx
+package {'nginx':
+  ensure => installed,
+}
+
+# Custom header
+file_line {'add_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $HOSTNAME;',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+#start service
+service {'nginx':
+  ensure    => 'running',
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File_line['add_header'],
+}
