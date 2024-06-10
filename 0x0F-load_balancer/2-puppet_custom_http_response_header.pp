6b98a8780cd99b1899d45e918ad4dac003a0a2bd
mariams58 <mariams58@yahoo.com>
2024-06-10 13:32:16 +0100

    load balancing - task 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,27 @@
+# Creates a custom hader in an nginx server
+#install Nginx in a server and configure it
+exec {'configure':
+  provider => shell,
+  user     => 'root',
+  command  => 'sudo apt-get -y update',
+}
+
+# Install Nginx
+package {'nginx':
+  ensure => installed,
+}
+
+# Custom header
+exec {'add_header':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'add_header X-Serverd-By $hostname;'
+}
+
+#start service
+service {'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
+}
