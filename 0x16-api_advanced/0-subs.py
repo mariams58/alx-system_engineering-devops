25c136350f524ea4f088846eb17a7b3194897d69
mariams58 <mariams58@yahoo.com>
2024-08-14 13:21:46 +0100

    new fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -25,7 +25,7 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     # Check if the request was successful (status code 200)
-    if response.status_code == 404:
+    if response.status_code != 200:
         return 0
     # Parse the JSON response
     data = response.json()
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -17,7 +17,7 @@ def top_ten(subreddit):
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
     # Base URL for the Reddit API
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
     # Set a custom User-Agent to avoid Too Many Requests error
     headers = {'User-Agent': '0x16-api:project:v1.0 (by /u/Broad_Advertising_21)'}
@@ -26,11 +26,12 @@ def top_ten(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     # Check if the request was successful (status code 200)
-    if response.status_code == 404:
+    if response.status_code != 200:
         print("None")
+        return
     # Parse the JSON response
-    data = response.json()
-    posts = data.get('data', {}).get('children', [])
+    # data = response.json()
+    posts = response.json().get('data', {}).get('children', [])
     # Print the titles of the top 10 posts
-    for post in posts:
+    for post in posts[:10]:
         print(post.get('data', {}).get('title'))
